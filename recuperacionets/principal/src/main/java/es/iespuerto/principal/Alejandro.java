005076350c12282e27c4ad8108e3e8e08d1a0930
alexey1254 <alxkill54@gmail.com>
2022-03-23 15:05:26 +0000

    Se añaden los constructores

--- recuperacionets/principal/src/main/java/es/iespuerto/principal/Alejandro.java
+++ recuperacionets/principal/src/main/java/es/iespuerto/principal/Alejandro.java
@@ -4,5 +4,41 @@ package es.iespuerto.principal;
  * @author Alejandro Marrero Cabrera
  */
 public class Alejandro {
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
 
+    /**
+     * Constructor por defecto
+     */
+    public Alejandro() {
+
+    }
+
+    /**
+     * Constructor con un parametro
+     * @param nom Nombre del alumno
+     */
+    public Alejandro(String nom) {
+
+    }
+
+    /**
+     * Constructor con dos parametros
+     * @param nom Nombre del alumno
+     * @param ap1 Primer apellido
+     */
+    public Alejandro(String nom, String ap1) {
+
+    }
+
+    /**
+     * Constructor con tres parametros
+     * @param nom Nombre del alumno
+     * @param ap1 Primer apellido
+     * @param ap2 Segundo apellido
+     */
+    public Alejandro(String nom, String ap1, String ap2) {
+
+    }
 }
